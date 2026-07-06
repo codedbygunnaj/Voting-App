4884bf472566548d54fb1a1521a7c54f8c0c1e84
codedbygunnaj <gunaj.chugh@gmail.com>
2026-07-06 10:13:45 +0530

    Integrated authentication for users and candidates using jwt tokens

---
+++ authentication/jwtUser.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
+require('dotenv').config();
+
+const jwtAuthMiddlewareUsers = (req,res,next)=>{
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) res.status(401).json({Error: 'User not authorized'});
+
+    try{
+        const decodedPayload = jwt.verify(token,`${process.env.SECRET_KEY_USER}`);
+        //attach user information to request object -> new key added to req
+        req.userInfo = decodedPayload;
+        next();
+    }catch(err){
+        console.log(err);
+        res.status(401).json({Error:'Unauthorized'});
+    }
+
+}
+
+const generateTokensUsers = function(userPayload){
+    return jwt.sign({userPayload},`${process.env.SECRET_KEY_USER}`,{expiresIn:'1y'})
+}
+
+module.exports = {jwtAuthMiddlewareUsers,generateTokensUsers}
