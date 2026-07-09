e934e66757b94069d0bdbd873efc7c0bce8fe23a
codedbygunnaj <gunaj.chugh@gmail.com>
2026-07-09 13:36:42 +0530

    Initialised frontend using React/Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = ()=>{
+    return(
+        <div style={{
+            padding:'20px',
+            fontFamily:'sans-serif'
+        }}>
+            <h1>Voter's Application</h1>
+            <p>Frontend setup is successfully running with TypeScript! 🎉</p>
+        </div>
+    );
+}
+
+export default App;
